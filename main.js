ea307126020b082fdc8b05e9db3ed95c82bae603
Jonathan Melendez <34495781+JonathanMelendez777@users.noreply.github.com>
2022-03-16 20:05:45 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -17,35 +17,35 @@ recognition.addEventListener('result', (e) => {
 
   if (e.results[0].isFinal) {
     console.log(text);
-    if (text.includes('led 1') || text.includes('Led 1')) {
+    if (text.includes('luz 1') || text.includes('Luz 1')) {
       if(led1.checked == 0){
         led1.checked = 1;
       }else{
         led1.checked = 0;
       }
     }
-    if (text.includes('led 2') || text.includes('Led 2')) {
+    if (text.includes('luz 2') || text.includes('Luz 2')) {
       if(led2.checked == 0){
         led2.checked = 1;
       }else{
         led2.checked = 0;
       }
     }
-    if (text.includes('led 3') || text.includes('Led 3')) {
+    if (text.includes('luz 3') || text.includes('Luz 3')) {
       if(led3.checked == 0){
         led3.checked = 1;
       }else{
         led3.checked = 0;
       }
     }
-    if (text.includes('led 4') || text.includes('Led 4')) {
+    if (text.includes('luz 4') || text.includes('Luz 4')) {
       if(led4.checked == 0){
         led4.checked = 1;
       }else{
         led4.checked = 0;
       }
     }
-    if (text.includes('led 5') || text.includes('Led 5')) {
+    if (text.includes('luz 5') || text.includes('Luz 5')) {
       if(led5.checked == 0){
         led5.checked = 1;
       }else{
